b4ebd1435db76033cac2ebca4fff5747aa5be184
Alexey T <support@uvviewsoft.com>
2026-05-26 18:34:16 +0300

    txt

--- cd_bk.py
+++ cd_bk.py
@@ -3,7 +3,7 @@ Authors:
     Andrey Kvichansky    (kvichans on github.com)
     Alexey Torgashin (CudaText)
 Version:
-    '0.9.16 2026-05-25'
+    '0.9.17 2026-05-26'
 ToDo: (see end of file)
 '''
 
